a36544260ea471d9e840d59e7aa01aac77c4750e
John <jrabraham1904@gmail.com>
2026-01-26 10:58:07 -0500

    parsing files better

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <getopt.h>
+#include <cassert>
 #include <format>
 #include <iostream>
 #include <fstream>
@@ -6,8 +7,14 @@
 #include <unordered_map>
 #include <cstdint>
 #include <sstream>
+#include <functional>
 
-std::regex var_regex("\\$var (wire|reg) [1-9][0-9]* .+ .+", std::regex_constants::extended);
+// Header
+typedef struct {
+    std::regex pattern;
+    std::function<void(std::string_view)> callback;
+} VcdKeyword;
+
 
 class Variable{
 private:
@@ -69,7 +76,7 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    std::ifstream vcd_file(static_cast<std::string>(file_name));
+    std::ifstream vcd_file(static_cast<std::string>(file_name), std::ios::in | std::ios::binary);
 
     if(!vcd_file.is_open()){
         std::cerr << "No such file\n";
@@ -78,16 +85,108 @@ int main(int argc, char** argv){
 
     std::string line;
 
-    while(std::getline(vcd_file, line)){
-        if(std::regex_match(line, var_regex)){
-            std::string _, type, size, vcd_name, var_name;
-            if(std::stringstream{line} >> _ >> type >> size >> vcd_name >> var_name){
-                std::cout << std::format("type: {} size: {} vcd_name: {} var_name {}\n", type, size, vcd_name, var_name);
-                var_map[vcd_name] = Variable(var_name);
-            }else{
-                throw std::runtime_error("Expected to read 5 arguments...");
+    std::vector<VcdKeyword> const keywords {
+        VcdKeyword{
+            .pattern=std::regex{"\\$timescale", std::regex_constants::extended}, 
+            .callback{[&](std::string_view s){
+                std::cout << std::format("Found {}", s);
+            }}},
+        VcdKeyword{
+            .pattern=std::regex{"\\$date", std::regex_constants::extended}, 
+            .callback{[&](std::string_view s){
+                std::cout << std::format("Found {}", s);
+            }}},
+        VcdKeyword{
+            .pattern=std::regex{"\\$comment", std::regex_constants::extended}, 
+            .callback{[&](std::string_view s){
+                std::cout << std::format("Found {}", s);
+            }}},
+        VcdKeyword{
+            .pattern=std::regex{"\\$version", std::regex_constants::extended}, 
+            .callback{[&](std::string_view s){
+                std::cout << std::format("Found {}", s);
+            }}},
+        VcdKeyword{
+            .pattern=std::regex{"\\$var", std::regex_constants::extended}, 
+            .callback{[&](std::string_view s){
+                std::string in(s);
+                std::string type, size, vcd_name, var_name;
+                if(std::stringstream{in} >> type >> size >> vcd_name >> var_name){
+                    std::cout << std::format("type: {} size: {} vcd_name: {} var_name {}\n", type, size, vcd_name, var_name);
+                    var_map[vcd_name] = Variable(var_name);
+                }else{
+                    throw std::runtime_error(std::format("Expected to read 4 arguments. Instead got {}", in));
+                }
+            }}}
+    };
+
+    std::size_t file_pos = 0;
+    std::function<void(std::size_t)> increment_file_pos = [&](std::size_t delta){
+        file_pos += delta;
+        std::cout << std::format("Seeking {}\n", file_pos);
+        vcd_file.seekg(file_pos);
+    };
+
+    static constexpr std::size_t BUFFER_SIZE = 500;
+    std::string buffer{};
+    // TODO(JOHN): this doesnt handle end of file conditions perfectly
+    while((buffer.resize(BUFFER_SIZE), vcd_file.read(buffer.data(), BUFFER_SIZE), vcd_file.gcount() > 0)){
+        buffer.resize(vcd_file.gcount());
+
+        std::regex const END_REGEX = std::regex{"\\$end"};
+
+        std::smatch start, end;
+        bool found_start = false;
+        std::function<void(std::string_view)> cb;
+        // search for the beginning keyword
+        for(auto const& [pattern, callback] : keywords){
+            // this is the dumbest thing ever, std::regex_search requires a full
+            // copy of the string, it cannot be passed a string_view >:(
+            if(std::regex_search(buffer, start, pattern)){
+                increment_file_pos(start.position());
+
+                // this line is bloat because it will do a large number of unecessary copying
+                // >:(
+                buffer = buffer.substr(start.position());
+                found_start = true;
+                cb = callback;
+                break;
             }
         }
+
+        // if we didn't find the start of a keyword, move to next portion of the buffer
+        if(!found_start){
+            std::cout << std::format("Didn't find start in {}\n", buffer);
+            increment_file_pos(buffer.size());
+            continue;
+        }
+
+        std::string_view data;
+
+        // if we did find the start of a keyword, search for the end
+        bool found_end = std::regex_search(buffer, end, END_REGEX);
+        if(!found_end && buffer.size() == BUFFER_SIZE){
+            throw std::runtime_error("BUFFER_SIZE is not large enough");
+        }else if(!found_end){
+            std::cout << std::format("Didn't find end in {}\n", buffer);
+            increment_file_pos(buffer.size());
+            continue;
+        }
+
+        // at this point we know that the operation has succeded
+        assert(found_end);
+
+        // use string view to avoid copying the data
+        // thank goodness for c++20
+        data = std::string_view(buffer.begin() + start.length(), buffer.begin() + end.position());
+
+        std::cout << "//////////////////////" << '\n';
+        cb(data);
+        std::cout << "//////////////////////" << '\n';
+
+        // this line should not be able to throw an exception since exceptions are thrown for pos > size not pos == size
+        buffer = buffer.substr(end.position() + end.length()); 
+        increment_file_pos(end.position() + end.length());
     }
 
     for(auto const& [key, val] : var_map){
