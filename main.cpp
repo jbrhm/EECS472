3f202448bbdb6e76b9746b5117fa16c738074787
John <jrabraham1904@gmail.com>
2026-01-26 22:35:37 -0500

    parsing files better

--- main.cpp
+++ main.cpp
@@ -19,37 +19,37 @@ typedef struct {
 typedef struct{
     int time;
     std::string value;
-} timestamp;
+} Timestamp;
 
 class Variable{
 private:
     std::string name;
-    std::vector<timestamp> value;
+    std::vector<Timestamp> value;
 
 public:
     Variable() = default;
     Variable(std::string name_in, std::string value_in = "x") : name{std::move(name_in)}, value{}{
-        value.push_back(timestamp{.time{}, .value{value_in}});
+        value.push_back(Timestamp{.time{}, .value{value_in}});
     }
     ~Variable() = default;
 
     void add_timestamp(int time, std::string value_in){
-        value.push_back(timestamp{.time=time, .value{value_in}});
+        value.push_back(Timestamp{.time=time, .value{value_in}});
     }
 
     [[nodiscard]] std::string const& get_name() const{
         return name;
     }
 
-    [[nodiscard]] std::vector<timestamp> const& get_value() const{
+    [[nodiscard]] std::vector<Timestamp> const& get_value() const{
         return value;
     }
 
-    [[nodiscard]] timestamp get_value(int time) const{
+    [[nodiscard]] Timestamp get_value(int time) const{
         int i = 0;
         bool found = false;
         for(; i < value.size(); ++i){
-            timestamp const& v = value[i];
+            Timestamp const& v = value[i];
 
             if(i + 1 < value.size() && value[i + 1].time > time){
                 found = true;
@@ -89,7 +89,6 @@ int main(int argc, char** argv){
 
         switch(c){
             case 'v':
-                std::cout << std::format("VCD file {}\n", optarg);
                 file_name = optarg;
                 break;
             case 'h':
@@ -117,13 +116,14 @@ int main(int argc, char** argv){
 
     static constexpr char const* END_KEYWORD = "$end";
 
+    std::vector<std::string> module_stack{};
     std::vector<VcdKeyword> const keywords {
         VcdKeyword{
             .pattern="$timescale",
             .callback{[&](std::ifstream& fs){
                 std::string str{};
                 while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
+                    // std::cout << str << '\n';
                 }
             }}},
         VcdKeyword{
@@ -131,7 +131,7 @@ int main(int argc, char** argv){
             .callback{[&](std::ifstream& fs){
                 std::string str{};
                 while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
+                    // std::cout << str << '\n';
                 }
             }}},
         VcdKeyword{
@@ -139,7 +139,7 @@ int main(int argc, char** argv){
             .callback{[&](std::ifstream& fs){
                 std::string str{};
                 while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
+                    // std::cout << str << '\n';
                 }
             }}},
         VcdKeyword{
@@ -147,32 +147,51 @@ int main(int argc, char** argv){
             .callback{[&](std::ifstream& fs){
                 std::string str{};
                 while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
+                    // std::cout << str << '\n';
                 }
             }}},
         VcdKeyword{
             .pattern="$scope",
             .callback{[&](std::ifstream& fs){
-                std::string str{};
-                while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
+                std::string _, name, end;
+                if(fs >> _ >> name){
+                    module_stack.push_back(name);
+                }else{
+                    throw std::runtime_error("Expected two tokens in scope statement...");
                 }
+
             }}},
         VcdKeyword{
             .pattern="$upscope", 
             .callback{[&](std::ifstream& fs){
-                std::string str{};
-                while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
-                }
+                std::string end;
+                assert(!module_stack.empty());
+
+                module_stack.pop_back();
+                assert(fs >> end && end == END_KEYWORD);
             }}},
         VcdKeyword{
             .pattern="$var", 
             .callback{[&](std::ifstream& fs){
-                std::string str{};
-                while(fs >> str && str != END_KEYWORD){
-                    std::cout << str << '\n';
+                std::string type, size, vcd_name, rtl_name, end;
+                if(fs >> type >> size >> vcd_name >> rtl_name){
+                    std::string module{};
+                    for(std::string const& s : module_stack){
+                        module += s + ".";
+                    }
+
+                    rtl_name = module + rtl_name;
+
+                    std::cout << std::format("Type: {} Size: {} VCD Name: {} RTL Name: {}\n", type, size, vcd_name, rtl_name);
+                }else{
+                    throw std::runtime_error("Expected four tokens in scope statement...");
                 }
+
+                // this statement exists to support the [31:0] syntax
+                if(fs >> end && end == END_KEYWORD){
+                    return;
+                }
+                assert(fs >> end && end == END_KEYWORD);
             }}}
     };
 
