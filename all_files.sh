e1b8dec3c5ecc0efa5aa5f6bfdbb4fdaf45d1d47
John <jrabraham1904@gmail.com>
2026-01-27 10:15:59 -0500

    script

--- all_files.sh
+++ all_files.sh
@@ -2,8 +2,8 @@
 
 echo "" > output
 
-for file in *.vcd; do
+for file in /home/jabra/EECS472/p2_472-w26.jabra/output/*.vcd; do
     echo "${file}"
-    ./a.out --vcd "${file}" >> output
+    ./a.out --vcd "${file}" --clock "testbench.verisimpleV.clock" >> output
 done
 
