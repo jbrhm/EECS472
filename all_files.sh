c2c7d51b0c089924ed01fa21dede8398ca704ddb
John <jrabraham1904@gmail.com>
2026-01-27 11:10:00 -0500

    update graphs

--- all_files.sh
+++ all_files.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo "" > output
+echo "" > output_file
 
-for file in /home/jabra/EECS472/p2_472-w26.jabra/output/*.vcd; do
+for file in output/*.vcd; do
     echo "${file}"
-    ./a.out --vcd "${file}" --clock "testbench.verisimpleV.clock" >> output
+    ./a.out --vcd "${file}" --clock "testbench.verisimpleV.clock" >> output_file
 done
-
--- parse_data_hazards.py
+++ parse_data_hazards.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 data_hazards = []
 branch_acc = []
-with open('output', 'r') as file: 
+with open('output_file', 'r') as file: 
     for i, line in enumerate(file):
         try:
             if i % 2 == 0:
@@ -15,15 +15,26 @@ with open('output', 'r') as file:
         except:
             pass
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(2, 1)
 
-ax.hist(data_hazards, bins=10)
+print(data_hazards)
+print(branch_acc)
+
+ax[0].hist(data_hazards, bins=10)
 
 # Add labels and a title
-ax.set_xlabel("# of Data Hazards")
-ax.set_ylabel("Frequency")
-ax.set_title("Data Hazards Per Benchmark")
-ax.set_xlim(0, 200000)
+ax[0].set_xlabel("# of Data Hazards")
+ax[0].set_ylabel("Frequency")
+ax[0].set_title("Data Hazards Per Benchmark")
+ax[0].set_xlim(0, 200000)
+
+ax[1].hist(branch_acc, bins=10)
+
+# Add labels and a title
+ax[1].set_xlabel("FTBNT Accuracy")
+ax[1].set_ylabel("Frequency")
+ax[1].set_title("FTBNT Accuracy (%)")
+ax[1].set_xlim(0, 1)
 
 # Display the plot
 plt.show()
